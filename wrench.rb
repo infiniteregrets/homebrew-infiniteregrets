2d47faf6bb66a478fdfceaacf6b02b34ed92da1a
Mehul Arora <mehul_arora@live.com>
2024-09-21 22:40:02 -0400

    fix wrong sha

--- wrench.rb
+++ wrench.rb
@@ -1,5 +1,5 @@
 class Wrench < Formula
-    desc "Wrench tool"
+    desc "Schema management tool for Cloud Spanner"
     homepage "https://github.com/cloudspannerecosystem/wrench"
     version "1.10.1"
     license "MIT"
@@ -7,10 +7,10 @@ class Wrench < Formula
     if OS.mac?
       if Hardware::CPU.arm?
         url "https://github.com/cloudspannerecosystem/wrench/releases/download/v#{version}/wrench-#{version}-darwin-arm64.tar.gz"
-        sha256 "65aac771e4c2667a89033a4f8e177b48a485b9389e636671b7c5033d90dd3942"
+        sha256 "2e1af2c0bc5e8a7a2415fbf6225245957a30822fd5a6310ad2cafdc2de6e0acb"
       else
         url "https://github.com/cloudspannerecosystem/wrench/releases/download/v#{version}/wrench-#{version}-darwin-amd64.tar.gz"
-        sha256 "2e1af2c0bc5e8a7a2415fbf6225245957a30822fd5a6310ad2cafdc2de6e0acb"
+        sha256 "65aac771e4c2667a89033a4f8e177b48a485b9389e636671b7c5033d90dd3942"
       end
     elsif OS.linux?
       if Hardware::CPU.arm?
@@ -29,4 +29,4 @@ class Wrench < Formula
     test do
       system "#{bin}/wrench", "--version"
     end
-  end
+end
